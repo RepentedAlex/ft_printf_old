93a61e3a18c931e893ef557f06028a5d07749e34
Alessandro Petitcollin <apetitco@paul-f4Ar7s8.clusters.42paris.fr>
2023-12-07 16:44:30 +0100

    Avancement petit a petit

--- ft_printf.h
+++ ft_printf.h
@@ -6,6 +6,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	ft_printf(const char *,...);
+int		ft_printf(const char *,...);
+int		ft_print_format(char c, va_list args);
+
+void	ft_print_char(int c);
+void	ft_print_number(int num, char *base);
+void	ft_print_pointer(int address);
+void	ft_print_string(char *str);
+void	ft_putchar(char c);
+void	ft_putnbr(int num, char *base);
 
 #endif
